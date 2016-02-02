44a7d88011093b4754f1c65802e292fea91c77ed
Oliver Kocsis <oliver.kocsis@gmail.com>
2016-02-02 20:53:28 +0100

    Finish bonus task

--- exam.R
+++ exam.R
@@ -46,17 +46,18 @@ ggplot(dt, aes(x = wt, y = mpg)) + geom_point() + geom_smooth(method = "lm", se 
 ## Install the ISLR package and use its Auto for the below exercises
 # install.packages("ISLR")
 library(ISLR)
-?Auto # Origin of car (1. American, 2. European, 3. Japanese)
+?Auto
+# Origin of car (1. American, 2. European, 3. Japanese)
 str(Auto)
 ## Build and visualize a decision tree to tell if a car was made in America, Europe or Japan
 library(rpart)
 set.seed(2016)
 auto <- Auto
 auto$origin <- as.factor(auto$origin) # classification
-l <- dim(Auto)[1]
+l <- dim(auto)[1]
 i <- sample(1:l, as.integer(l * 0.75))
-train = auto[i, -9]
-test = auto[-i, -9]
+train <- auto[i, -9]
+test <- auto[-i, -9]
 ct <- rpart(origin ~ ., data = train)
 summary(ct)
 plot(ct)
@@ -64,7 +65,8 @@ text(ct)
 table(test$origin, predict(ct, newdata = test, type = "class")) # Confusion matrix
 ## Apply k-means or hierarchical clustering on the dataset to split the observations into 3 groups
 # k-means
-kc <- kmeans(auto[,1:6], centers = 3) # Not using year, origin, and name
+# Not using year, origin, and name
+kc <- kmeans(auto[,1:6], centers = 3) 
 summary(kc)
 table(kc$cluster, auto$origin)
 # hierarchical
@@ -76,3 +78,21 @@ cn <- cutree(hc, k = 3)
 table(cn, auto$origin)
 
 ## Bonus exercise: train a reasonable k-NN or other ML model classifying cars as American VS other origin (target for AUC > 0.95)
+# install.packages("gbm")
+library(gbm)
+auto <- Auto[,1:6]
+auto$american <- Auto$origin == 1
+str(auto)
+summary(auto)
+l <- dim(auto)[1]
+i <- sample(1:l, as.integer(l * 0.75))
+train <- auto[i,]
+test <- auto[-i,]
+md <- gbm(american ~ ., data = train, distribution = "bernoulli", n.trees = 100, interaction.depth = 10, shrinkage = 0.05, cv.folds = 3)
+gbm.perf(md, plot.it = TRUE)
+md
+p <- predict(md, test, n.trees = 100) 
+conf <- table(ifelse(p > 0,1,0), test$american)
+conf
+# Accuracy
+(conf[2,2] + conf[1,1]) / (conf[1,1] + conf[1,2] + conf[2,2] + conf[2,1])
